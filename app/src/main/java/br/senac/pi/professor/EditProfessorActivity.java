f0b15905e5f168c05b8af5d7b12bdc48ba5cd643
joaofcs87 <joaofcss@yahoo.com.br>
2015-11-23 16:26:09 -0300

    continuacao1

--- app/src/main/java/br/senac/pi/professor/EditProfessorActivity.java
+++ app/src/main/java/br/senac/pi/professor/EditProfessorActivity.java
@@ -1,19 +1,81 @@
 package br.senac.pi.professor;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import br.senac.pi.professor.domain.ProfessorDB;
 
 public class EditProfessorActivity extends AppCompatActivity {
-    /*Nessa Activity, a partir do id capturado na ListaProfessorActivity
-    * Faz-se um casting de dois EditText (nome, disciplina) e tbm faz-se um casting de um TextView
-    * que recebe o id (_id) do registro que será editado
-    * Depois é feito o update a partir de uma chamada do objeto db, onde os 'SET values'
-    * serão os valores que estiverem nos dois casting de EditText*/
+
+    private ProfessorDB professorDB;
+    private SQLiteDatabase db;
+    private EditText edtProfessor, edtDisciplina;
+    private TextView txtProfessor;
+    private String id;
+    private Cursor cursor;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_professor);
+
+        id = getIntent().getStringExtra("id");
+
+        professorDB = new ProfessorDB(this);
+
+        txtProfessor = (TextView) findViewById(R.id.txtIdProfessor);
+        edtProfessor = (EditText) findViewById(R.id.edtNomeProfessor);
+        edtDisciplina = (EditText) findViewById(R.id.edtDisciplinaProfessor);
+
+        cursor =  loadProfessor(Integer.parseInt(id));
+
+        txtProfessor.setText(cursor.getString(cursor.getColumnIndexOrThrow("_id")));
+        edtProfessor.setText(cursor.getString(cursor.getColumnIndexOrThrow("nome")));
+        edtDisciplina.setText(cursor.getString(cursor.getColumnIndexOrThrow("disciplina")));
+
+        findViewById(R.id.btnEditarProfessor).setOnClickListener(editarProfessor());
+    }
+
+    private Cursor loadProfessor(int id){
+        db = professorDB.getWritableDatabase();
+        String[] campos = {"_id", "nome", "disciplina"};
+        String whereArgs = String.valueOf(id);
+        cursor = db.query("professores", campos, "_id = " + whereArgs, null, null, null, null);
+        if (cursor != null){
+            cursor.moveToFirst();
+        }
+        db.close();
+        return cursor;
+    }
+
+    private View.OnClickListener editarProfessor(){
+        return new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                db = professorDB.getWritableDatabase();
+
+                ContentValues values = new ContentValues();
+
+                String whereArgs = id;
+
+
+                values.put("nome", edtProfessor.getText().toString());
+                values.put("disciplina", edtDisciplina.getText().toString());
+
+                db.update("professores", values, "_id = " + whereArgs, null);
+                Toast.makeText(getApplicationContext(),getString(R.string.edit_ok),Toast.LENGTH_LONG).show();
+                db.close();
+            }
+        };
     }
 }
--- app/src/main/java/br/senac/pi/professor/ListaProfessorActivity.java
+++ app/src/main/java/br/senac/pi/professor/ListaProfessorActivity.java
@@ -1,18 +1,28 @@
 package br.senac.pi.professor;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.CursorAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
 
 import br.senac.pi.professor.domain.ProfessorDB;
 
 public class ListaProfessorActivity extends AppCompatActivity {
 
-    SQLiteDatabase db;
+    private CursorAdapter dataSource;
+    private SQLiteDatabase database;
+    private static final String campos[] = {"_id", "nome", "disciplina",};
+    ListView listView;
     ProfessorDB professorDB;
 
     @Override
@@ -20,29 +30,74 @@ public class ListaProfessorActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_professor);
 
+        listView = (ListView) findViewById(R.id.listViewProfessor);
         professorDB = new ProfessorDB(this);
-
-        findViewById(R.id.btnListarProfessorCadastrado).setOnClickListener(listarProfessores());
+        database = professorDB.getWritableDatabase();
+        findViewById(R.id.btnListarProfessorCadastrado).setOnClickListener(listarProfessor());
+        listView.setOnItemClickListener(itemProfessor());
     }
 
-    private View.OnClickListener listarProfessores(){
+    private View.OnClickListener listarProfessor() {
         return new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Cursor professores = database.query("professores", campos, null, null, null, null, null);
+                if (professores.getCount() > 0) {
 
-                ListView listProfessor = (ListView) findViewById(R.id.listViewProfessor);
+                    dataSource = new SimpleCursorAdapter(ListaProfessorActivity.this, R.layout.row_professor, professores, campos, new int[]{R.id.txtIdProfessor,R.id.txtNomeProfessor, R.id.txtDisciplina});
+                    listView.setAdapter(dataSource);
+                } else {
+                    Toast.makeText(ListaProfessorActivity.this, getString(R.string.zero_professores), Toast.LENGTH_LONG).show();
+                }
+            }
+        };
+    }
 
-                /*Nesse listView é exibido todos os professores cadastrados
-                * Cada linha (registro) mostrado ao ser clicado é exibido um AlertDialog,
-                * onde tem-se duas opções: PositiveButton(Editar) ou NegativeButton(Excluir)
-                * -Se for clicado em Editar: Implementa-se uma Intent para uma outra Activity de Editar
-                * (EditProfessor), sendo enviado o id do registro
-                * -Se for clicado em Excluir: Nesta Activity mesmo é feita a instrução de Excluir,
-                 * usando a propria instancia do ProfessorDB, também tendo como parâmetro o id do registro
-                 * (_id)*/
+    private AdapterView.OnItemClickListener itemProfessor() {
+        return new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                final long itemSelecionado = id;
+
+                final int posicao = position;
+                AlertDialog.Builder builder = new AlertDialog.Builder(ListaProfessorActivity.this);
+                builder.setTitle(getString(R.string.opcoes));
+                builder.setMessage(getString(R.string.msg_opcoes));
+                builder.setPositiveButton(getString(R.string.editar_professor), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+
+                        String codigo;
+
+                        Cursor professor = database.query("professores", campos, null, null, null, null, null);
+                        professor.moveToPosition(posicao);
+                        codigo = professor.getString(professor.getColumnIndexOrThrow("_id"));
+
+                        Intent intent = new Intent(getApplicationContext(), EditProfessorActivity.class);
+                        intent.putExtra("id", codigo);
+                        startActivity(intent);
+                        finish();
+
+                    }
+                });
+                builder.setNegativeButton(getString(R.string.excluir_professor), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+
+                        Log.i("professores", "ID do Professor: " + itemSelecionado);
+
+                        String whereArgs = String.valueOf(itemSelecionado);
+
+                        database.delete("professores", "_id = " + whereArgs, null);
+                        Toast.makeText(getApplicationContext(),getString(R.string.delete_ok), Toast.LENGTH_LONG).show();
+
+                    }
+                });
+                AlertDialog dialog = builder.create();
+                dialog.show();
+
 
             }
         };
     }
 }
-
